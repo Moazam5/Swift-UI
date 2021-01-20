80e129fbad567ecde3d81d19fa3c344862b315d2
Moazam5 <m.moazam29@gmail.com>
2021-01-20 14:28:08 -0500

    Initial Commit

---
+++ SwiftUI-1/SwiftUI_1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_1App.swift
+//  SwiftUI-1
+//
+//  Created by Moazam Mir on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
